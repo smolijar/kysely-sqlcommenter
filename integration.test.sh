fccceeeb3bc88ea6ee9938ef21d63273074e76b0
Jaroslav Šmolík <smolijar@gmail.com>
2024-02-05 20:27:14 +0100

    ✅ Add (failing) integration test for mod aug

---
+++ integration.test.sh
@@ -0,0 +1,11 @@
+npm link
+rm -rf tmp;
+mkdir tmp;
+cp src/integration.ts.template tmp/main.ts;
+cd tmp;
+npm init -y;
+npm install tsx kysely;
+npm link kysely-sqlcommenter;
+npx tsc --init;
+npx tsx main.ts;
+# TODO: test tsc
